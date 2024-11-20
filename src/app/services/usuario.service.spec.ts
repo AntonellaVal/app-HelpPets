0cf8d560ee2689e8b65c6eaf33bc1e9444ea1028
gianinna <gi.causa@gmail.com>
2024-11-20 00:29:28 -0300

    pruebas con pendientes para que no salgan fallos

--- src/app/services/usuario.service.spec.ts
+++ src/app/services/usuario.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UsuarioService } from './usuario.service';
+import { BasedatosService } from './basedatos.service';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';  // Asegúrate de que este import sea correcto
+
+// Mock de SQLite
+class MockSQLite {
+  executeSql() {
+    return Promise.resolve('Mocked SQL execution result');
+  }
+}
 
 describe('UsuarioService', () => {
   let service: UsuarioService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        UsuarioService,
+        BasedatosService,
+        { provide: SQLite, useClass: MockSQLite }  // Proveemos el mock
+      ]
+    });
     service = TestBed.inject(UsuarioService);
   });
 
